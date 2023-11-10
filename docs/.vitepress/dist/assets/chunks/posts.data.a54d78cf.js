6a929ec1d2c0480c7fc253ea1f2606c9dc793a21
R-koma <ryomakomazawa@gmail.com>
2023-11-10 14:47:47 +0900

    change content

---
+++ docs/.vitepress/dist/assets/chunks/posts.data.a54d78cf.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('[{"frontmatter":{"theme":"page","title":"初投稿","next":false,"prev":false,"date":"2023/11/9"},"url":"/posts/first.html"}]');export{t as d};
--- docs/.vitepress/dist/assets/chunks/posts.data.c1a7b0d7.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('[{"frontmatter":{"theme":"page","title":"このブログについて","next":false,"prev":false,"date":"2023/11/9"},"url":"/posts/des.html"},{"frontmatter":{"theme":"page","title":"初投稿","next":false,"prev":false,"date":"2023/11/9"},"url":"/posts/first.html"}]');export{t as d};
